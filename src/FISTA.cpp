718adecaf66a59e3d90ee75c0250b1a58227f3c4
awkwardboy <xiaoyukeen@outlook.com>
2018-08-01 22:23:07 +0800

    daily update

--- src/FISTA.cpp
+++ src/FISTA.cpp
@@ -1,7 +1,6 @@
 #include <Rcpp.h>
-// #include <Rinternals.h>
+#include <RcppEigen.h>
 #include <vector>
-#include "Eigen/Dense"
 #include <iostream>
 
 using namespace Rcpp;
@@ -9,44 +8,11 @@ using Eigen::MatrixXd;
 using Eigen::MatrixXi;
 using Eigen::Map;
 
-// void load(const int n, const int p, std::vector<int> &);
-// void loadFromR(const int n, const int p);
-/*
-int main()
-{
-    const int n = 3;
-    const int p = 4;
-    std::vector<int> x;
-    for(int i=0; i < 12; i++)
-    {
-        x.push_back(i);
-    }
-  load(n, p, x);
-}
-*/
-/*
-void load(const int n, const int p, std::vector<int> &x) 
-{
-    Map<MatrixXi> X(x.data(), n, p);
-    std::cout << X*2<< std::endl;
-}
- */
-
-/*
-MatrixXd loadRdata(Map<MatrixXd> &x)
-{
-    std::cout << x << std::endl;
-    return x.transpose();
-}
-*/
 
 
-extern "C" SEXP loadR(SEXP x, SEXP n, SEXP p){
-    // Map<MatrixXd> X(as<Map<MatrixXd> >(x));
-    Vector<traits::r_sexptype_traits<double>::rtype> vec(x);
-    int nc = as<int>(n);
-    int pc = as<int>(p);
-    Map<MatrixXd> X(vec.begin(), nc, pc);
+extern "C" SEXP loadR(SEXP x){
+    Map<MatrixXd> X(as<Map<MatrixXd> >(x));
     NumericMatrix Xr(wrap(X));
     return Xr;
 }
+
--- src/make.sh
+++ src/make.sh
@@ -1,3 +1,3 @@
 #！ /bin/bash
-g++ -I/Library/Frameworks/R.framework/Resources/include -DNDEBUG  -I/usr/local/include -I/usr/local/include/freetype2 -I/opt/X11/include -I/Library/Frameworks/R.framework/Versions/3.3/Resources/library/Rcpp/include -fPIC  -Wall -mtune=core2 -g -O2 -c FISTA.cpp -o FIS∑TA.o
-g++ -dynamiclib -Wl,-headerpad_max_install_names -undefined dynamic_lookup -single_module -multiply_defined suppress -L/Library/Frameworks/R.framework/Resources/lib -L/usr/local/lib -o FISTA.so FISTA.o -F/Library/Frameworks/R.framework/.. -framework R -Wl,-framework -Wl,CoreFoundation
+g++ -I/usr/local/Cellar/r/3.5.1/lib/R/include -DNDEBUG -I/usr/local/lib/R/3.5/site-library/Rcpp/include -I/usr/local/lib/R/3.5/site-library/RcppEigen/include -fPIC  -Wall -mtune=core2 -g -O2 -c FISTA.cpp -o FISTA.o
+g++ -dynamiclib -Wl,-headerpad_max_install_names -undefined dynamic_lookup -single_module -multiply_defined suppress -L/usr/local/opt/gettext/lib -L/usr/local/opt/readline/lib -L/usr/local/lib -L/usr/local/Cellar/r/3.5.1/lib/R/lib -o FISTA.so FISTA.o -L/usr/local/Cellar/r/3.5.1/lib/R/lib -lR -lintl -Wl,-framework -Wl,CoreFoundation
